efacf2a3266b249ee2285630faf8e9389483875d
mguery <mguery@effisoft-group.com>
2022-10-14 11:40:24 +0200

    Opérations de base SQl

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+_
+/src/test/java/com/mycompany/tennis/AppTest.java,6\b\6b748ab9ff804ef7a08b946d63f0f4d9457648e5
--- src/main/java/com/mycompany/tennis/App.java
+++ src/main/java/com/mycompany/tennis/App.java
@@ -1,23 +1,22 @@
 package com.mycompany.tennis;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class App 
 {
     public static void main(String... args){
         Connection conn = null;
         try {
-            //Seulement avant Java 7/JDBC 4
-            //Class.forName(DRIVER_CLASS_NAME);
-
-            //MySQL driver MySQL Connector
+            // MySQL driver MySQL Connector
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false","root","root");
-            //Oracle Driver officiel OJDBC Thin
-            //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tennis","COURSDB","COURSDB");
-            //Postgres Driver officiel
-            //conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","COURSDB","COURSDB");
+            Statement statement = conn.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT NOM, PRENOM, ID FROM JOUEUR");
+            while(rs.next()) {
+                final String nom = rs.getString("NOM");
+                final String prenom = rs.getString("PRENOM");
+                final Long id = rs.getLong("ID");
+                System.out.println(id + " : " + prenom + " " + nom);
+            }
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
